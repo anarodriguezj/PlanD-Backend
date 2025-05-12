981e033395bf42077e4ae79b639f1f2c484e6857
gvidalquadras <202203923@alu.comillas.edu>
2025-05-12 19:03:17 +0200

    Models

--- subastas/models.py
+++ subastas/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from users.models import CustomUser
+from django.core.validators import MinValueValidator, MaxValueValidator
+
 
 class Category(models.Model):
 
-    name = models.CharField(max_length=50, blank=False, unique=True)
+    '''Modelo de categoría'''
+
+    name = models.CharField(max_length=50, blank=False, unique=True) # Nombre de la categoría
 
     class Meta:
         ordering=('id',)
@@ -15,6 +19,7 @@ class Category(models.Model):
 class Auction(models.Model):
 
     '''Modelo de subasta'''
+
     title = models.CharField(max_length=150) # Título
     description = models.TextField() # Descripción
     closing_date = models.DateTimeField() # Fecha límie para su cierre
@@ -22,27 +27,65 @@ class Auction(models.Model):
     thumbnail = models.URLField() # Campo de imagen
     price = models.DecimalField(max_digits=10, decimal_places=2) # Precio de salida
     stock = models.IntegerField() # Stock
-    rating = models.DecimalField(max_digits=3, decimal_places=2) # Valoración
     category = models.ForeignKey(Category, related_name='subastas', on_delete=models.CASCADE) # Categoría
     brand = models.CharField(max_length=100) # Marca
     auctioneer = models.ForeignKey(CustomUser, related_name='auctions', on_delete=models.CASCADE) # Usuario que crea la subasta
 
     class Meta:
         ordering=('id',) 
+
+    def average_rating(self):
+
+        '''Función para el cálculo de la media de las valoraciones de la subasta'''
+
+        ratings = self.ratings.all()
+        if ratings.exists():
+            return sum(rating.value for rating in ratings)/ratings.count()
+        return 1.0
     
     def __str__(self):  
         return self.title
 
 
 class Bid(models.Model):
+
+    '''Modelo de puja'''
     
-    amount = models.DecimalField(max_digits=10, decimal_places=2)
-    timestamp = models.DateTimeField(auto_now_add=True)
-    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE)  # Aquí usamos CustomUser
-    auction = models.ForeignKey(Auction, related_name='pujas', on_delete=models.CASCADE)
+    amount = models.DecimalField(max_digits=10, decimal_places=2) # Cantidad que se puja
+    timestamp = models.DateTimeField(auto_now_add=True) # Fecha en la que se hace la puja
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE) # Usuario que puja
+    auction = models.ForeignKey(Auction, related_name='pujas', on_delete=models.CASCADE) # Subasta a la que se puja
 
     class Meta:
         ordering = ('id',)
 
     def __str__(self):
         return f"{self.user.username} bid {self.amount} on {self.auction.title}"
+    
+class Rating(models.Model):
+
+    '''Modelo de valoración'''
+
+    rating = models.IntegerField(validators=[MinValueValidator(1), MaxValueValidator(5)]) # Valoración
+    auction = models.ForeignKey(Auction, related_name="ratings", on_delete= models.CASCADE) # Subasta que se valora
+    user = models.ForeignKey(CustomUser, related_name="ratings", on_delete=models.CASCADE) # Usuario que valora
+    
+    class Meta:
+        ordering=('id',)
+
+    def __str__(self):
+        return f"{self.user.username} rated {self.rating} for {self.auction.title}" 
+
+class Comment(models.Model):
+
+    '''Modelo de comentario'''
+
+    title = models.CharField(max_length=200) # Título
+    text = models.TextField() # Texto
+    auction = models.ForeignKey(Auction, related_name="comments", on_delete=models.CASCADE,) # Subasta sobre la que se comenta
+    user = models.ForeignKey(CustomUser, related_name="comments", on_delete=models.CASCADE) # Usuario que comenta
+    creation_date = models.DateTimeField(auto_now_add=True) # Fecha de creación
+    modification_date = models.DateTimeField(auto_now=True) # Fecha de modificación
+    
+    def __str__(self):
+        return f"{self.user.username} commented {self.title} for {self.auction.title}" 
--- users/models.py
+++ users/models.py
@@ -2,6 +2,9 @@ from django.db import models
 from django.contrib.auth.models import AbstractUser
 
 class CustomUser(AbstractUser):
+
+    '''Modelo de usuario personalizado que hereda de AbstractUser'''
+
     birth_date = models.DateField()
     locality = models.CharField(max_length=100, blank=True)
     municipality = models.CharField(max_length=100, blank=True)
